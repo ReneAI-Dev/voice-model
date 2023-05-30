9f7df7cf30f61b3e0a199bfca62da270d575f056
Trevor Oler <18trevor3695@gmail.com>
2023-05-30 17:42:19 -0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,13 +1,22 @@
 #!/bin/bash
 
 # navigate to the directory containing your preprocessing script
-cd /path/to/preprocessing
+cd ./
+
+# Download the Mozilla TTS repository as a ZIP file
+curl -LOk https://github.com/mozilla/TTS/archive/refs/heads/main.zip
+
+# Unzip the downloaded file
+unzip main.zip
+
+# Delete the downloaded ZIP file
+rm main.zip
 
 # run your preprocessing script
 python preprocess.py
 
 # navigate to the directory containing the TTS code
-cd /path/to/TTS
+cd TTS-main
 
 # run the training script with your configuration file
 python TTS/bin/train.py --config_path /path/to/config.json
